b17827e2c6410ce1b5a82381aa0b1a5e403c78b4
Deuxts <ariasdavidv@gmail.com>
2021-10-07 17:58:15 -0500

    refactor del proyecto, se cambio las rutas de algunos archivos y tambien se dividieron en modulos, esto con el fin de modificar con facilidad en el futuro, tambien se añadio un documento para las llamadas a MongoDB, en caso de que cambie algo se haran las modificaciones desde dicho archivo

---
+++ src/lib/db-operations.ts
@@ -0,0 +1,61 @@
+import { filter } from 'compression';
+import { Db } from 'mongodb';
+/**
+ * Para obtenee el ID que usaremos de un nuevo usuario
+ * @param database base de datos con la que estamos trabajando
+ * @param collection coleccion de datos del ultimo element
+ * @param sort tipo de orden a usar
+ * 
+ */
+export const asignDocumentId = async (
+    database: Db,
+    collection: string,
+    sort: object = { registerDate: -1 }
+) => {
+    const lastElement = await database
+        .collection(collection)
+        .find()
+        .limit(1)
+        .sort(sort)
+        .toArray();
+    if (lastElement.length === 0) {
+        return 1;
+    }
+    return lastElement[0].id + 1;
+};
+
+
+export const findOneElement = async (
+    database: Db,
+    collection: string,
+    filter: object
+) => {
+    return database
+        .collection(collection)
+        .findOne(filter);
+};
+
+export const insertOneElement = async (
+    database: Db,
+    collection: string,
+    document: object
+) => {
+    return await database.collection(collection).insertOne(document);
+};
+
+export const insertManyElements = async (
+    database: Db,
+    collection: string,
+    documents: Array<object>
+) => {
+    return await database.collection(collection).insertMany(documents);
+};
+
+export const findElements = async (
+    database: Db,
+    collection: string,
+    filter: object = {}
+) => {
+    return await database.collection(collection).find(filter).toArray();
+};
+
